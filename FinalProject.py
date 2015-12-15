30f6e96206d6287385c55e9ab50fe75b75c90191
CriticalD20 <jamesnapier@hanovernorwichschools.org>
2015-12-15 14:57:42 -0500

    Updated from Brython Server: December 15, 2015 2:57:42 PM EST

--- FinalProject.py
+++ FinalProject.py
@@ -1,5 +1,5 @@
-print("Welcome to the Text-based The Long Night Recreation.")
-print("A strange electromagnetic storm has crashed your plane in Northern Canada. How long can you survive?")
+one = input("Welcome to the Text-based The Long Night Recreation.")
+two = input("A strange electromagnetic storm has crashed your plane in Northern Canada. How long can you survive?")
 Time = 0
 Freeze = 10
 
@@ -10,8 +10,17 @@ def SeeWolf():
     print("You notice a lupine shape run past a few meters from you. It does not give you a second thought, and quickly leaves your sight.")
 
 def WolfAttack():
-    print(
+    print("You hear howling, as the crunches of multiple paws racing through the snow surround you completely. The eyes of the wolves all around you spell certain death.")
+
+        
+def Freeze():
+    print("As your vision dims and the cold completely fills your body, you fall, and lay in the snow. You feel tired, and your eyes slowly close. You are dead.")
+
+def House():
+    print("As you crest the hill, a small lodge comes into your vision.")
+
+
     
-print("You fin yourself in a clearing 200 meters in diameter. It is surrounded by snow-laden conifers, and no buildings are in sight.")
-print("You are standing in the middle of the clearing, next to your downed plane, flames still licking across the chasis, and a rut in the snow lead about 40 meters to the right.")
-first_choice = input(" You check your compass, and you are facing North. What will you do? (a) Go North (b) Go East (c) Go South (d) Go West (e) Search the Plane (f) Wait an hour")
+three = input("You find yourself in a clearing 200 meters in diameter. It is surrounded by snow-laden conifers, with no buildings in sight.")
+four = input("You are standing in the middle next to your downed plane, flames still licking across the chasis, and a rut in the snow leads from the plane to about 40 meters to the right.")
+first_choice = input(" You check your compass, and you are facing North. What will you do? \n(a) Go North \n(b) Go East \n(c) Go South \n(d) Go West \n(e) Search the Plane \n(f) Wait an hour")
