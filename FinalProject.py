37fbf607632c449b106d119760483efc326d11d9
CriticalD20 <jamesnapier@hanovernorwichschools.org>
2015-12-15 14:21:22 -0500

    Updated from Brython Server: December 15, 2015 2:21:22 PM EST

--- FinalProject.py
+++ FinalProject.py
@@ -1,1 +1,12 @@
+rint("Welcome to the Text-based The Long Night Recreation.")
+print("A strange electromagnetic storm has crashed your plane in Northern Canada. How long can you survive?")
+Time = 0
+Freeze = 10
 
+def Wait():
+    print("The snow whistles, and the wind whips up your hair as you sit and wait. Sound of the forest can be heard. Howls echo through the trees, and chirps of birds are a rare occurance.")
+    
+def SeeWolf
+print("You fin yourself in a clearing 200 meters in diameter. It is surrounded by snow-laden conifers, and no buildings are in sight.")
+print("You are standing in the middle of the clearing, next to your downed plane, flames still licking across the chasis, and a rut in the snow lead about 40 meters to the right.")
+first_choice = input(" You check your compass, and you are facing North. What will you do? (a) Go North (b) Go East (c) Go South (d) Go West (e) Search the Plane (f) Wait an hour")
