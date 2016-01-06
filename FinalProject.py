e944720f5c0b4e8692e65a747c1ca1cd84c3d986
CriticalD20 <jamesnapier@hanovernorwichschools.org>
2016-01-06 13:47:25 -0500

    Updated from Brython Server: 1/6/2016 1:47:24 PM

--- FinalProject.py
+++ FinalProject.py
@@ -34,47 +34,88 @@ def Wait():
     else:
         print("That is not a valid choice. Please resart the program, and make sure not to include anything other than the letter of the choice in the input box.")
         
-    
+def Nothing():
+    print("As you trudge through the woods, nothing seems out of the ordinary.")
+    Freezing()
+    CardinalChoice2()
+def CardinalChoice2():
+    print("You sense of direction has been lost form walking through the woods. Which direction would you like to try to go?\n(a) North\n(b) East\n(c) South\n(d) West")
+    if CardinalChoice2 == "a":
+        Freezing()
+        SeeWolf()
+    elif CardninalChoice2 == "b" or == "c" or == "c":
+        Freezing()
+        CardinalChoice2()
+    else:
+        print("That is not a valid choice. Please resart the program, and make sure not to include anything other than the letter of the choice in the input box.")
+def PossibleWolf2():
+    choiceW = input("After seeing the wolf you come to a clearing. Which direction would you like to go?\n(a) North\n(b) East\n(c) South\n(d) West")
+    if choiceW.lower == "a":
+        print("You trudge through the snow past trees and small snow-mounds towards North.")
+        Freezing()
+        WolfAttack()
+    elif choiceW == "b":
+        print("You trudge through the snow past trees and small snow-mounds towards East.")
+        House()
+    elif choiceW == "c":
+        print("You trudge through the snow past trees and small snow-mounds towards South.")
+        Freezing()
+        WolfAttack()
+    elif choiceW == "d":
+        print("You trudge through the snow past trees and small snow-mounds towards West.")
+        Freezing()
+        WolfAttack()
+    else:
+        print("That is not a valid choice. Please resart the program, and make sure not to include anything other than the letter of the choice in the input box.")
+def PossibleWolf():
+    choiceW = input("After seeing the wolf you come to a clearing. Which direction would you like to go?\n(a) North\n(b) East\n(c) South\n(d) West")
+    if choiceW.lower == "a":
+        print("You trudge through the snow past trees and small snow-mounds towards North.")
+        Freezing()
+        WolfAttack()
+    elif choiceW == "b":
+        print("You trudge through the snow past trees and small snow-mounds towards East.")
+        Freezing()
+        WolfAttack()
+    elif choiceW == "c":
+        print("You trudge through the snow past trees and small snow-mounds towards South.")
+        Freezing()
+        WolfAttack()
+    elif choiceW == "d":
+        print("You trudge through the snow past trees and small snow-mounds towards West.")
+        Freezing()
+        PossibleWolf2()
+    else:
+        print("That is not a valid choice. Please resart the program, and make sure not to include anything other than the letter of the choice in the input box.")
 def SeeWolf():
     print("You notice a lupine shape run past a few meters from you. It does not give you a second thought, and quickly leaves your sight.")
     Freezing()
-    
+    PossibleWolf()
 def WolfAttack():
-    print("You hear howling, as the crunches of multiple paws racing through the snow surround you completely. The eyes of the wolves all around you spell certain death.")
-    
-    Freezing()
+    print("You trudge through the snow, and all of a sudden a noise break the silence. You hear howling, as the crunches of multiple paws racing through the snow surround you completely. The eyes of the wolves all around you spell certain death.")
     
 def House():
-    print("As you crest the hill, a small lodge comes into your vision.")
+    print("As you crest the hill, a small lodge comes into your vision. You race towards the warmth and your only way of survival.")
     
-    Freezing()
-def Nightfall():
-    print("The sun slowly drops below the treeline, and the darkness comes. You cannot see very well now, and the forest sounds seem much louder and sharper than normal.")
-    
-    Freezing()
 def North():
     print("You trudge through the snow past trees and small snow-mounds towards North.")
-    
-    
     Freezing()
+    Nothing()
 def South():
     print("You trudge through the snow past trees and small snow-mounds towards South.")
-    
-    
     Freezing()
+    SeeWolf()
 def East():
     print("You trudge through the snow past trees and small snow-mounds towards East.")
-    
-    
     Freezing()
+    Nothing()
 def West():
     print("You trudge through the snow past trees and small snow-mounds towards West.")
-    
-    
     Freezing()
+    SeeWolf()
 def SearchPlane():
     print("You trudge through the snow to the plane, only to find that the fire does not allow you to access the plane. However, it does seem warmer here.")
-    Freezing()
+    
 def CardinalChoice():
     Cardinal_Direction = input("You check your compass, and you are facing North. What will you do? \n(a) Go North \n(b) Go East \n(c) Go South \n(d) Go West")
     if Cardinal_Direction.lower() == "a":
@@ -96,17 +137,18 @@ if first_choice.lower() == "a":
     North()
 elif first_choice.lower() == "b":
     East()
-    Freezing()
+ 
 elif first_choice.lower() == "c":
     South()
-    Freezing()
+    
 elif first_choice.lower() == "d":
     West()
-    Freezing()
+    
 elif first_choice.lower() == "e":
     SearchPlane()
+    Wait()
 elif first_choice.lower() == "f":
     Wait()
-    Freezing()
+    
 else:
     print("That is not a valid choice. Please resart the program, and make sure not to include anything other than the letter of the choice in the input box.")
