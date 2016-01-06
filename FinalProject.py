56dffb363304641054d4ecee82e00a90ed6ddbfe
CriticalD20 <jamesnapier@hanovernorwichschools.org>
2016-01-06 13:56:40 -0500

    Updated from Brython Server: 1/6/2016 1:56:39 PM

--- FinalProject.py
+++ FinalProject.py
@@ -1,3 +1,4 @@
+
 one = input("Welcome to the Text-based The Long Night Recreation.\n(Press Enter)")
 two = input("A strange electromagnetic storm has crashed your plane in Northern Canada. How long can you survive? \n(Press Enter)")
 
